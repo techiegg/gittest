c8ea098932acff4c2824ad5f8638efebac34a9d1
Sunny Gupta <sunnygupta.rd@gmail.com>
2022-07-12 11:24:01 +0530

    Second commit

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
 
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+	return 'Hello World'
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# main driver function
+if __name__ == '__main__':
 
-
-# Press the green button in the gutter to run the script.
-if __name__ =='__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+	# run() method of Flask class runs the application
+	# on the local development server.
+	app.run()
