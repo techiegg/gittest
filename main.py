7c509d95e2dc5167da5d0a63ddb05060c665e894
Sunny Gupta <sunnygupta.rd@gmail.com>
2022-07-16 14:59:40 +0530

    Isort fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 from flask import Flask
-import random
+
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
