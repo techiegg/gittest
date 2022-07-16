6f5c2ee37b6da09904d2b30e3be0c013e61dab10
Sunny Gupta <sunnygupta.rd@gmail.com>
2022-07-16 14:52:08 +0530

    flake8 fix

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask import Flask
 # current module (__name__) as argument.
 app = Flask(__name__)
 
+
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
